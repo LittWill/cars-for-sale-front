19cf450b867400d901035b6902969c63df4117df
Wilson Almeida <wilsonfmz14@gmail.com>
2024-03-19 19:39:48 -0300

    Criação do projeto; criação do componente card-veiculo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardVeiculoComponent } from "./card-veiculo/card-veiculo.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, CardVeiculoComponent]
 })
 export class AppComponent {
   title = 'cars-for-sale-front';
---
+++ src/app/card-veiculo/card-veiculo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-veiculo',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-veiculo.component.html',
+  styleUrl: './card-veiculo.component.scss'
+})
+export class CardVeiculoComponent {
+
+}
