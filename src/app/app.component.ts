82481f78c6573162f16c7651cb121d11f9a5499e
Wilson Almeida <wilsonfmz14@gmail.com>
2024-03-22 23:48:18 -0300

    Estilização da navbar; criação do carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardVeiculoComponent } from "./card-veiculo/card-veiculo.component";
 import { ListarVeiculosComponent } from './listar-veiculos/listar-veiculos.component';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, CardVeiculoComponent, ListarVeiculosComponent]
+    imports: [RouterOutlet, CardVeiculoComponent, ListarVeiculosComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'cars-for-sale-front';
